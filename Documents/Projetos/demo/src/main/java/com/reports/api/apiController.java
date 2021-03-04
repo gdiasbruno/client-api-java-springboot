f59c7530a00c5f70d8d093e20540e03accfa209b
Bruno Gonçalves Dias <x371544@rede.sp>
2021-03-04 18:19:46 -0300

    rota post

--- Documents/Projetos/demo/src/main/java/com/reports/api/apiController.java
+++ Documents/Projetos/demo/src/main/java/com/reports/api/apiController.java
@@ -1,46 +1,55 @@
 package com.reports.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.json.JSONObject;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class apiController {
 
-    public URL setup() {
-        System.setProperty("http.proxyHost", "127.0.0.1");
-        System.setProperty("http.proxyPort", "3128");
 
-        URL url = null;
-        try {
-            url = new URL("https://api.kanye.rest/");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        try {
-            URLConnection con = url.openConnection();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return url;
-    }
 
     @Autowired
     private RestTemplate restTemplate;
 
-    private static String url = "setup()";
+    private static String url = "https://restcountries.eu/rest/v2/all";
+    private static String url1 = "http://formulario.smads.prefeitura.sp.gov.br/index.php/admin/remotecontrol";
 
     @GetMapping("/countries")
     public List<Object> getCountries(){
         Object[] countries = restTemplate.getForObject(url, Object[].class);
         return Arrays.asList(countries);
     }
+
+    @GetMapping("/lime")
+    public Object postLimeSurvey(){
+
+        String[] admin = {"admin","admin_c2o2v2s"};
+
+        JSONObject jo = new JSONObject();
+        jo.put("method", "get_session_key");
+        jo.put("id", 1);
+        jo.put("params", admin);
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<String>(jo.toString(), headers);
+        System.out.println(entity);
+
+        Object response = restTemplate.postForObject(url1, entity, Object.class);
+        System.out.println(response);
+        return response;
+    }
+
 }
